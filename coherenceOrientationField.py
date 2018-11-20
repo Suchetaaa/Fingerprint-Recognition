368f24b81cddad9b2ec555d4d11e0ca8ddf2c9ad
kateverdeen <41998666+kateverdeen@users.noreply.github.com>
2018-11-20 16:41:32 +0530

    Add files via upload

--- coherenceOrientationField.py
+++ coherenceOrientationField.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
+from math import pi
 
 
 #Read the image
-I = cv2.imread();
+I = cv2.imread("dummy.tif");
+I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+print(I.dtype)
+#print(I.shape)
 
+#I = np.ones((100,100))
 #Gaussian Filter
 G = cv2.GaussianBlur(I,(5,5),0)
 
@@ -12,9 +17,9 @@ G = cv2.GaussianBlur(I,(5,5),0)
 Gx = cv2.Sobel(G,cv2.CV_64F,1,0,ksize=3)
 Gy = cv2.Sobel(G,cv2.CV_64F,0,1,ksize=3)
 
-J1 = 2*(np.dot(Gx, Gy))
-J2 = np.dot(Gx, Gx) - np.dot(Gy, Gy)
-J3 = np.dot(Gx, Gx) + np.dot(Gy, Gy)
+J1 = 2*(np.multiply(Gx, Gy))
+J2 = np.multiply(Gx, Gx) - np.multiply(Gy, Gy)
+J3 = np.multiply(Gx, Gx) + np.multiply(Gy, Gy)
 
 Gradient = np.sqrt(J3)
 
@@ -26,7 +31,7 @@ sigma_J2 = cv2.filter2D(J2,-1,anisotropy_filter)
 sigma_J3 = cv2.filter2D(J2,-1,anisotropy_filter)
 
 theta_bar = 0.5 * np.arctan(np.divide(sigma_J1, sigma_J2))
-
+#theta_bar = np.zeros(sigma_J1.shape)
 theta = (pi/2) + theta_bar
 
 gt = 0.15
@@ -36,11 +41,11 @@ Grad_min = np.amin(Gradient)
 Gth = gt * (abs(Grad_max) - abs(Grad_min)) + abs(Grad_min)
 
 
-block_coherence = np.sqrt(np.dot(sigma_J1, sigma_J1) + np.dot(sigma_J2, sigma_J2))
+block_coherence = np.sqrt(np.multiply(sigma_J1, sigma_J1) + np.multiply(sigma_J2, sigma_J2))
 block_coherence = np.divide(block_coherence, sigma_J3)
-
+#block_coherence = np.zeros(sigma_J1.shape)
 ###############try to optimise this part of the code
-compare_matrix = sigma_J3 / (16*16) < np.dot(Gth, Gth)
+compare_matrix = sigma_J3 / (16*16) < np.multiply(Gth, Gth)
 
 for i in range(0, compare_matrix.shape[0]):
 	for j in range(0, compare_matrix.shape[1]):
@@ -49,7 +54,8 @@ for i in range(0, compare_matrix.shape[0]):
 
 ############################
 connectivity = 8
-output = cv2.connectedComponentsWithStats(block_coherence, connectivity)
+block_coherence = block_coherence.astype(np.int8)
+output = cv2.connectedComponentsWithStats(block_coherence, connectivity, cv2.CV_8S)
 labels = output[1]
 stats = output[2]
 foreground_label = np.argmax(stats, axis =0)[4]
