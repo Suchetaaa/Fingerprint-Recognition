140b5b5ea439056232ba4033157788c94c2f662d
kateverdeen <neharikajali@yahoo.co.in>
2018-11-21 21:34:05 +0530

    Pushing I don't know what

--- Core detection /Normalisation.py
+++ Core detection /Normalisation.py
@@ -4,15 +4,26 @@ import math
 
 def Normalisation (test_image, M_0, V_0):
 	Mean = mean.test_image()
+	V_i = test_image.var()
+	
 	output_image = np.matrix(test_image.size)
 	Size = test_image.shape()
-	V_i = test_image.var()
+	
+
+	"""
 	for i in range (0, Size[0]):
 		for j in range (0, Size[1]):
 			if (test_image[i][j] > Mean):
 				output_image[i][j] = M_0 + math.sqrt(V_0*math.pow((test_image[i][j]-Mean),2)/V_i)
 			else:
 				output_image[i][j] = M_0 - math.sqrt(V_0*math.pow((test_image[i][j]-Mean),2)/V_i) 
+	"""
+
+	for i,j in np.argwhere(test_image > Mean):
+		output_image[i][j] = M_0 + math.sqrt(V_0*math.pow((test_image[i][j]-Mean),2)/V_i)
+
+	for i,j in np.argwhere(test_image <= Mean):
+		output_image[i][j] = M_0 - math.sqrt(V_0*math.pow((test_image[i][j]-Mean),2)/V_i)
 
 
 	return output_image
