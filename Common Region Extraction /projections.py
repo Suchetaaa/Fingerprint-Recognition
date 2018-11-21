556f75ea5f3697a9c2a012f887d0fdf6124be2e8
Suchetaaa <sucheta317@gmail.com>
2018-11-21 19:27:46 +0530

    Common region extraction working

--- Common Region Extraction /projections.py
+++ Common Region Extraction /projections.py
@@ -1,44 +1,65 @@
 import numpy as np 
 import cv2 
 import math 
+from PIL import Image
 
-input_img_path = To be written
-reg_img_path = To be written 
-input_img = cv2.imread(input_img_path)
-ref_img = cv2.imread(ref_img_path)
+input_img = cv2.imread("Input Image.tif", 0)
+reg_img = cv2.imread("Registered Image.tif", 0)
 
-rows, cols = input_img.size
-max_height_reduction = 50
-max_width_reduction = 50
+rows, cols = input_img.shape
+max_height_reduction = 20
+max_width_reduction = 20
 
 best_norm = -99999
-best_height_reduction = 50
-best_width_reduction = 50
+best_height_reduction_top = 50
+best_height_reduction_bottom = 50
+best_width_reduction_left = 50
+best_width_reduction_right = 50
 
-for x in xrange(0,max_height_reduction):
-	for y in yrange(0,max_width_reduction):
-		input_dummy = np.zeroes((rows - 2*x, cols - 2*y))
-		reg_dummy = np.zeros((rows - 2*x, cols - 2*y))
-		input_dummy = input_img[x : (rows - x - 1), y : (cols - y - 1)]
-		reg_dummy = ref_img[x : (rows - x - 1), y : (cols - y - 1)]
+# i - top, j - bottom
+# k - left, l - right
 
-		input_row_sum = input_dummy.sum(axis = 1)
-		input_col_sum = input_dummy.sum(axis = 0)
-		reg_row_sum = ref_dummy.sum(axis = 1)
-		reg_col_sum = ref_dummy.sum(axis = 0)
+for i in range(0,max_height_reduction) :
+	for j in range(0,max_height_reduction) :
+		for k in range(0,max_width_reduction) :
+			for l in range(0,max_width_reduction) :
+				input_dummy = np.zeros((rows - i - j, cols - k - l))
+				reg_dummy = np.zeros((rows - i - j, cols - k - l))
+				input_dummy = input_img[i : (rows - i - j - 1), k : (cols - k - l - 1)]
+				reg_dummy = reg_img[i : (rows - i - j - 1), k : (cols - k - l - 1)]
 
-		input_array = [input_row_sum, input_col_sum]
-		reg_array = [reg_row_sum, reg_col_sum]
-		dist = np.linalg.norm(input_array - reg_array)
+				input_row_sum = input_dummy.sum(axis = 1)
+				input_col_sum = input_dummy.sum(axis = 0)
+				reg_row_sum = reg_dummy.sum(axis = 1)
+				reg_col_sum = reg_dummy.sum(axis = 0)
 
-		if (dist < best_norm) : 
-			best_norm = dist 
-			best_height_reduction = x
-			best_width_reduction = y
+				input_row_sum = np.reshape(input_row_sum, (input_row_sum.shape[0], 1))
+				input_col_sum = np.reshape(input_col_sum, (input_col_sum.shape[0], 1))
+				reg_row_sum = np.reshape(reg_row_sum, (reg_row_sum.shape[0], 1))
+				reg_col_sum = np.reshape(reg_col_sum, (reg_col_sum.shape[0], 1))
 
-input_final = np.zeroes((rows - 2*best_height_reduction, cols - 2*best_width_reduction))
-reg_final = np.zeroes((rows - 2*best_height_reduction, cols - 2*best_width_reduction))
+				input_array = np.concatenate((input_row_sum, input_col_sum), axis = 0)
+				reg_array = np.concatenate((reg_row_sum, reg_col_sum), axis = 0)
+				input_array = np.asarray(input_array)
+				reg_array = np.asarray(reg_array)
+				norm_arr = np.subtract(input_array, reg_array)
+				dist = np.linalg.norm(norm_arr)
 
-input_final = input_img[best_height_reduction : (rows - best_height_reduction - 1), best_width_reduction : (cols - best_width_reduction - 1)]
-reg_final = reg_img[best_height_reduction : (rows - best_height_reduction - 1), best_width_reduction : (cols - best_width_reduction - 1)]
+				if (dist < best_norm) : 
+					best_norm = dist 
+					best_height_reduction_top = i
+					best_height_reduction_bottom = j
+					best_width_reduction_left = k
+					best_width_reduction_right = l
+
+input_final = np.zeros((rows - best_height_reduction_top - best_height_reduction_bottom, cols - best_width_reduction_left - best_width_reduction_right))
+reg_final = np.zeros((rows - best_height_reduction_top - best_height_reduction_bottom, cols - best_width_reduction_left - best_width_reduction_right))
+
+input_final = input_img[best_height_reduction_top : (rows - best_height_reduction_top - best_height_reduction_bottom - 1), best_width_reduction_left : (cols - best_width_reduction_left - best_width_reduction_right - 1)]
+reg_final = reg_img[best_height_reduction_top : (rows - best_height_reduction_top - best_height_reduction_bottom - 1), best_width_reduction_left : (cols - best_width_reduction_left - best_width_reduction_right - 1)]
+
+input_img_final = Image.fromarray(input_final)
+input_img_final.show()
+reg_img_final = Image.fromarray(reg_final)
+reg_img_final.show()
 
