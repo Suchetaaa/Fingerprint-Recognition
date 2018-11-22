55ec549290dc8aece2ecf27229e7cc31ceae749f
kateverdeen <neharikajali@yahoo.co.in>
2018-11-23 02:40:44 +0530

    Database 4 is also ready

--- Core detection /singularPointDetection.py
+++ Core detection /singularPointDetection.py
@@ -15,24 +15,26 @@ j = 0
 k = 0
 l = 0
 
-image_names1 = os.listdir('/home/neharika/Desktop/DIP/Project/DB1_B')
-image_names1.sort()
+# image_names1 = os.listdir('/home/neharika/Desktop/DIP/Project/DB1_B')
+# image_names1.sort()
 # image_names2 = os.listdir('DB2_B/')
+# image_names1.sort()
 # image_names3 = os.listdir('DB3_B/')
-# image_names4 = os.listdir('DB4_B/')
+image_names4 = os.listdir('/home/neharika/Desktop/DIP/Project/DB4_B')
+image_names4.sort()
 
 singularPoint_1 = (-1) * np.ones((80, 2))
 singularPoint_2 = (-1) * np.ones((80, 2))
 singularPoint_3 = (-1) * np.ones((80, 2))
 singularPoint_4 = (-1) * np.ones((80, 2))
 
-for images in tqdm(image_names1):
-	print(images)
-	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB1_B/' + images)
-	singularPoint_1[i,0] = a
-	singularPoint_1[i, 1] = b
-	print((a,b))
-	i = i+1
+# for images in tqdm(image_names1):
+# 	print(images)
+# 	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB1_B/' + images)
+# 	singularPoint_1[i,0] = a
+# 	singularPoint_1[i, 1] = b
+# 	print((a,b))
+# 	i = i+1
 
 	
 # for images in tqdm(image_names2):
@@ -47,14 +49,16 @@ for images in tqdm(image_names1):
 # 	singularPoint_3[k, 1] = b
 # 	k = k+1
 	
-# for images in tqdm(image_names4):
-# 	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB4_B' + images)
-# 	singularPoint_4[l,0] = a
-# 	singularPoint_4[l, 1] = b
-# 	l = l+1
+for images in tqdm(image_names4):
+	print(images)
+	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB4_B/' + images)
+	singularPoint_1[i,0] = a
+	singularPoint_1[i, 1] = b
+	print((a,b))
+	i = i+1
 	
 
-np.save('singularPoint_1.npy', singularPoint_1)
+#np.save('singularPoint_1.npy', singularPoint_1)
 # np.save('singularPoint_2.npy', singularPoint_2)
 # np.save('singularPoint_3.npy', singularPoint_3)
-# np.save('singularPoint_4.npy', singularPoint_4)
+np.save('singularPoint_4.npy', singularPoint_4)
