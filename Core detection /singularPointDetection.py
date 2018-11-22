0fadce0906e8bc22a9152f62c4189b063c0a0f31
kateverdeen <neharikajali@yahoo.co.in>
2018-11-23 01:21:13 +0530

    database creation rectified--up and working

--- Core detection /singularPointDetection.py
+++ Core detection /singularPointDetection.py
@@ -16,6 +16,7 @@ k = 0
 l = 0
 
 image_names1 = os.listdir('/home/neharika/Desktop/DIP/Project/DB1_B')
+image_names1.sort()
 # image_names2 = os.listdir('DB2_B/')
 # image_names3 = os.listdir('DB3_B/')
 # image_names4 = os.listdir('DB4_B/')
@@ -26,26 +27,28 @@ singularPoint_3 = (-1) * np.ones((80, 2))
 singularPoint_4 = (-1) * np.ones((80, 2))
 
 for images in tqdm(image_names1):
-	(a, b) = singularPointDetection(images)
+	print(images)
+	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB1_B/' + images)
 	singularPoint_1[i,0] = a
 	singularPoint_1[i, 1] = b
+	print((a,b))
 	i = i+1
 
 	
 # for images in tqdm(image_names2):
-# 	(a, b) = singularPointDetection(images)
+# 	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB2_B' + images)
 # 	singularPoint_2[j,0] = a
 # 	singularPoint_2[j, 1] = b
 # 	j = j+1
 
 # for images in tqdm(image_names3):
-# 	(a, b) = singularPointDetection(images)
+# 	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB3_B' + images)
 # 	singularPoint_3[k,0] = a
 # 	singularPoint_3[k, 1] = b
 # 	k = k+1
 	
 # for images in tqdm(image_names4):
-# 	(a, b) = singularPointDetection(images)
+# 	(a, b) = singularPointDetection('/home/neharika/Desktop/DIP/Project/DB4_B' + images)
 # 	singularPoint_4[l,0] = a
 # 	singularPoint_4[l, 1] = b
 # 	l = l+1
