98665387e4b0becc8ab9a35b56e3330733a95965
Swadha8 <swadhasanghvi4@gmail.com>
2018-11-22 23:05:55 +0530

    Update coherenceOrientationField.py

--- Core detection /coherenceOrientationField.py
+++ Core detection /coherenceOrientationField.py
@@ -1,46 +1,47 @@
 import cv2
 import numpy as np
 from math import pi
+import scipy as sp
+import math
+import numpy as np 
+from scipy import stats
+from direction_field import *
 
+def Singular_point_detection(img_2):
+	#Read the image
 
+	img_3 = img_2
+	img_2 = cv2.cvtColor(img_2, cv2.COLOR_BGR2GRAY)
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+	I = cv2.dilate(img_2,kernel,iterations = 1)
 
-
-#Read the image
-#I = cv2.imread("/home/neharika/Desktop/DIP/Project/dummy4.tif");
-
-
-
-def coherenceOrientationField(I):
-	w1 = 16
-	I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
-	#print(I.dtype)
-	#print(I.shape)
-	#I = np.ones((100,100))
-	#Gaussian Filter
-
+	## Gaussian Blurring to remove noise
 	G = cv2.GaussianBlur(I,(5,5),0)
 
 	#Sobel Operator
 	Gx = cv2.Sobel(G,cv2.CV_64F,1,0,ksize=3)
 	Gy = cv2.Sobel(G,cv2.CV_64F,0,1,ksize=3)
 
+
+	# derivative calculation Gradient
 	J1 = 2*(np.multiply(Gx, Gy))
 	J2 = np.multiply(Gx, Gx) - np.multiply(Gy, Gy)
 	J3 = np.multiply(Gx, Gx) + np.multiply(Gy, Gy)
-
 	Gradient = np.sqrt(J3)
-	cv2.imshow('gradient square', I)
-	cv2.waitKey(0)
 
-
-	#anisotropy orientation estimation is done using convoltion with a 16*16 ones filter
-
-	anisotropy_filter = np.ones((16,16))
+	# gradient smoothening
+	w1 = 16
+	anisotropy_filter = np.ones((w1,w1))
 	sigma_J1 = cv2.filter2D(J1,-1,anisotropy_filter)
 	sigma_J2 = cv2.filter2D(J2,-1,anisotropy_filter)
 	sigma_J3 = cv2.filter2D(J3,-1,anisotropy_filter)
 
+	# less smoothened  Gradient
+	w2 = 10
+	anisotropy_filter_0 = np.ones((w2,w2))
+	sigma_J3_0 = cv2.filter2D(J3,-1,anisotropy_filter_0)
 
+	# Direction Calculation
 	theta_bar = 0.5 * np.arctan(np.divide(sigma_J1, sigma_J2))
 	for i,j in np.argwhere(sigma_J2 == 0):
 		if (sigma_J1[i,j] > 0):
@@ -50,78 +51,75 @@ def coherenceOrientationField(I):
 		else:
 			theta_bar[i,j] = 0
 
-	theta = (pi/2) + theta_bar
+	theta_dash = (pi/2) + theta_bar
+
+
+	## threshold matrix on coherence
 	gt = 0.10
 	Grad_max = np.amax(Gradient)
 	Grad_min = np.amin(Gradient)
-
 	Gth = gt * (abs(Grad_max) - abs(Grad_min)) + abs(Grad_min)
-	## threshold matrix
 
+	## calculation of block coherence
 	block_coherence = np.sqrt(np.multiply(sigma_J1, sigma_J1) + np.multiply(sigma_J2, sigma_J2))
 	block_coherence = np.divide(block_coherence, sigma_J3)
 	for i,j in np.argwhere(sigma_J3 < Gth*Gth*(w1*w1)):
 		block_coherence[i,j] = (-1)
 
-	cv2.imshow('gradient square', block_coherence)
-	cv2.waitKey(0)
-
-	print(block_coherence)
-	print(np.argwhere(block_coherence > 0))
-	"""
+	#Mask calculation!!	
 	a = np.zeros(sigma_J1.shape)
 	for i,j in np.argwhere(block_coherence > 0):
 		a[i,j] = 1
-	"""
-	#cv2.imshow('gradient square', a)
-	#cv2.waitKey(0)
+	for i,j in np.argwhere(block_coherence < 0):
+		a[i,j] = -1
 
-	#block_coherence = np.zeros(sigma_J1.shape)
-	###############try to optimise this part of the code
-	# compare_matrix = sigma_J3 / (16*16) < np.multiply(Gth, Gth)
+	#### 8 field direction calculation
+	theta = Eight_field_Direction(img_3)
 
-	# for i in range(0, compare_matrix.shape[0]):
-	# 	for j in range(0, compare_matrix.shape[1]):
-	# 		if compare_matrix[i][j] == 1:
-	# 			block_coherence[i][j] = -1
-
-	# ############################
-	# connectivity = 8
-	# block_coherence = block_coherence.astype(np.int8)
-	# output = cv2.connectedComponentsWithStats(block_coherence, connectivity, cv2.CV_8S)
-	# labels = output[1]
-	# stats = output[2]
-	# foreground_label = np.argmax(stats, axis =0)[4]
-
-	block_coherence_dash = (-1)*np.ones(block_coherence.shape)
-	foreground_mask = np.zeros(I.shape)
-
-	#for i,j in np.argwhere(labels == foreground_label):
-	for i,j in np.argwhere(block_coherence > 0):
-		block_coherence_dash[i,j] = block_coherence[i,j]
-		foreground_mask[i,j] = 1
-
-	kernel_closing = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
-	foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ELLIPSE, kernel_closing)
-	foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ELLIPSE, kernel_closing)
-	foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ELLIPSE, kernel_closing)
-	foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ELLIPSE, kernel_closing)
-	foreground_mask = cv2.morphologyEx(foreground_mask, cv2.MORPH_ELLIPSE, kernel_closing)
-
-	cv2.imshow('foreground_mask', foreground_mask)
-	cv2.waitKey(0)
+	poincare_image = np.zeros(a.shape)
+	for i,j in np.argwhere(a != -1):
+		store = np.zeros([3,3])
+		if ((i-1 >= 0) and (i+1 <= a.shape[0]-1) and (j-1 >= 0) and (j+1 <= a.shape[1]-1)):
+			store[0,0] = theta[i-1,j] - theta[i-1, j-1]
+			store[0,1] = theta[i-1, j+1] - theta[i-1, j]
+			store[0,2] = theta[i,j+1] - theta[i-1, j+1]
+			store[1,2] = theta[i+1, j+1] - theta[i, j+1]
+			store[2,2] = theta[i+1,j] - theta[i+1, j+1]
+			store[2,1] = theta[i+1, j-1] - theta[i+1, j]
+			store[2,0] = theta[i, j-1] - theta[i+1, j-1]
+			store[1,0] = theta[i-1, j-1] - theta[i, j-1]
+			
+			for x,y in np.argwhere(store > pi/2):
+				store[x,y] = pi - store[x,y]
+				
+			for x,y in np.argwhere(store < (-0.5)*pi):
+				store[x,y] = pi + store[x,y]
 
 
-	"""
+			poincare_image[i,j] = store.sum()/(2*pi)
+	f = 0
+	delta_image = np.zeros(img_2.shape)
+	#### Detecting all the singularities
+	# for x,y in np.argwhere(poincare_image == 0.5):
+	# 	img_3[x,y] = [0,0,255]
+	# 	img_3[x+1, y] = [0,0,255]
+	# 	img_3[x, y+1] = [0,0,255]
+	# 	img_3[x+1, y+1] = [0,0,255]
 
-	for i in range(0, block_coherence.shape[0]):
-		for j in range(0, block_coherence.shape[1]):
-			if labels[i][j] == foreground_label:
-				block_coherence_dash = block_coherence[i][j]
-	"""			
+	# Detecting the maximum gradient 
+	# CORE is the point of maximum ridge
 
-	return foreground_mask
+	index_1, index_2 = [-1,-1]
+	max_gradient = 0	
+	for x,y in np.argwhere(poincare_image == 0.5):
+		if(sigma_J3_0[x,y] > max_gradient):
+			index_1,index_2 = x,y
+			max_gradient = sigma_J3_0[x,y]
 
 
-#foreground_mask =  coherenceOrientationField(I)
-#print(foreground_mask)
+	singular_point_Index = []
+	singular_point_Index.append(index_1)
+	singular_point_Index.append(index_2)
+
+	return singular_point_Index
+
