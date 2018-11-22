a48e3b09886e2acaa159dba1056a20b37bdfeeae
Swadha8 <swadhasanghvi4@gmail.com>
2018-11-22 23:06:34 +0530

    Update singularPointDetection.py

--- Core detection /singularPointDetection.py
+++ Core detection /singularPointDetection.py
@@ -1,28 +1,11 @@
 import cv2
 import numpy as np
 from math import pi
+from coherenceOrientationField import *
 
+def singularPointDetection(path):	
+	#returns the singular points
+	image = cv2.imread(path)
+	return Singular_point_detection(image)
 
-# import Normalisation
-import coherenceOrientationField
-import direction_field
-import poincare_index
-
-
-##Read the image
-#I = cv2.imread("/home/neharika/Desktop/DIP/Project/dummy4.tif");
-
-def singularPointDetection(I):
-
-	#I = Normalisation.Normalisation(I)
 	
-	foreground_mask = coherenceOrientationField.coherenceOrientationField(I)
-
-	# cv2.imshow('foreground_mask', foreground_mask)
-	# cv2.waitKey(0)
-
-	directionField = direction_field.direction_field(I, foreground_mask)
-
-	singularPoints = poincare_index.poincare_index(I, foreground_mask, directionField)
-
-	return singularPoints
