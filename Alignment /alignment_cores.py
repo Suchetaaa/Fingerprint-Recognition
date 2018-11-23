ae6bb20f69ddcbe330593c2e5a8a352ceebffc7f
Suchetaaa <sucheta317@gmail.com>
2018-11-23 13:08:30 +0530

    Cores alignment updated

--- Alignment /alignment_cores.py
+++ Alignment /alignment_cores.py
@@ -8,16 +8,18 @@ def align_cores(input_core_coord, reg_img_coord, input_img_path, reg_img_path):
 	reg_img = cv2.imread(reg_img_path, 0)
 	# reg_img_arr = np.asarray(reg_img)
 	# input_arr = np.asarray(input_img)
-
+	print "lala"
+	print input_core_coord
+	print reg_img_coord
 	disp_x =  input_core_coord[0] - reg_img_coord[0] 
 	disp_y = input_core_coord[1] - reg_img_coord[1] 
 	rows, cols = reg_img.shape
-	transformation = np.float32([[1, 0, disp_x], [0, 1, disp_y]])
+	transformation = np.float32([[1, 0, disp_y], [0, 1, disp_x]])
 	displaced_reg = np.zeros((rows, cols))
 	displaced_reg = cv2.warpAffine(reg_img, transformation, (cols, rows))
-	print displaced_reg.shape
+	# print displaced_reg.shape
 	img = Image.fromarray(displaced_reg)
-	img.show()
+	# img.show()
 	print "alignment cores done"
 	return displaced_reg
 	
